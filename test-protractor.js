7f1682a081e49c93e95bebfe15675cd48fb1de8a
dasha akhten <dasha.akhten@gmail.com>
2022-01-09 13:08:17 -0800

    fix spec

---
+++ test-protractor.js
@@ -0,0 +1,17 @@
+describe("Protractor baby steps", function () {
+  it("Open Angular js website", function () {
+    //write your protractor raw code
+  });
+
+  //Each it block will be called as a spec
+
+  it("close browswer", function () {
+    // code to close browser
+  });
+});
+
+// describe -- test suite
+
+// it -- test case
+
+// spec -- test file
--- todo-spec.js
+++ todo-spec.js
@@ -7,13 +7,13 @@ describe("angularjs homepage todo list", function () {
     );
     element(by.css('[value="add"]')).click();
 
-    var todoList = element.all(by.repeater("todo in todoList.todos"));
+    const todoList = element.all(by.repeater("todo in todoList.todos"));
     expect(todoList.count()).toEqual(3);
     expect(todoList.get(2).getText()).toEqual("write first protractor test");
 
     // You wrote your first test, cross it off the list
     todoList.get(2).element(by.css("input")).click();
-    var completedAmount = element.all(by.css(".done-true"));
+    const completedAmount = element.all(by.css(".done-true"));
     expect(completedAmount.count()).toEqual(2);
   });
 });
